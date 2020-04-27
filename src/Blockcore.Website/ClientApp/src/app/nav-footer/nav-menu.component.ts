cfb8f1722cf5692f35ebee45d937c62e612049e6
SondreB <sondre@outlook.com>
2020-04-27 16:05:24 +0200

    Implement dark mode toggling in a different manner
    - Improves the dark mode, relying on less overrides and filters.

--- src/Blockcore.Website/ClientApp/src/app/nav-footer/nav-menu.component.ts
+++ src/Blockcore.Website/ClientApp/src/app/nav-footer/nav-menu.component.ts
@@ -28,15 +28,27 @@ export class NavFooterComponent {
       // Toggle the dark mode.
       this.darkMode = !this.darkMode;
 
+      // const trans = () => {
+      //    document.documentElement.classList.add('transition');
+      //    window.setTimeout(() => {
+      //       document.documentElement.classList.remove('transition');
+      //    }, 500);
+      // };
+
+      // trans();
+
       // Update the UI.
       this.updateMode();
    }
 
    updateMode() {
+
+
       if (this.darkMode) {
-         this.renderer.addClass(document.body, 'dark');
+         this.renderer.setAttribute(document.documentElement, 'data-theme', 'dark'); //  .addClass(document.body, 'dark');
       } else {
-         this.renderer.removeClass(document.body, 'dark');
+         this.renderer.setAttribute(document.documentElement, 'data-theme', 'light');
+         // this.renderer.removeClass(document.body, 'dark');
       }
    }
 }

7abfb29709dfcc4c950725758bcbfa4806099395
Milad Raeisi <6504337+miladsoft@users.noreply.github.com>
2022-09-13 04:43:32 +0430

    Added the wallet page and responsive menu (#34)
    * Added wallet page
    
    * Make the main menu responsive
    
    * Completed the wallet page and responsive menu
    
    * Disabled the hamburger-button menu
    
    * Update the home screen texts
    
    * Update the privacy policy and link to it from footer
    
    * Remove sponsors component and menu item
    
    * Shuffle the products under platform
    
    * Add links and improve texts on home and platform pages
    
    * Improve styling and merge stylesheets
    
    * Improve menu layout and update text for wallet
    
    Co-authored-by: SondreB <sondre@outlook.com>

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-nav-menu',
-  templateUrl: './nav-menu.component.html'
+   selector: 'app-nav-menu',
+   templateUrl: './nav-menu.component.html'
 })
 export class NavMenuComponent {
 
+   menu() {
+      let navigation = document.querySelector(".navigation");
+      navigation.classList.toggle("active");
+   }
 }
